f514227f02152df9fa462d2602a1da9083b4dc66
burundu4ok2000 <burundu4ok2000@gmail.com>
2025-12-09 21:16:04 +0300

    feat(seo): Added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-
+    site: 'https://emozika-spb.ru', // TODO: Update with actual domain
+    integrations: [sitemap()],
 });
